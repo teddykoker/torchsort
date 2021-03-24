df7f96617d6011bc6167b176e5eb4dc988c5cdf4
Teddy Koker <teddy.koker@gmail.com>
2021-03-23 20:01:26 -0400

    version 0.1.1

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.md") as f:
 
 setup(
     name="torchsort",
-    version="0.1.0",
+    version="0.1.1",
     description="Differentiable sorting and ranking in PyTorch",
     author="Teddy Koker",
     url="https://github.com/teddykoker/torchsort",
