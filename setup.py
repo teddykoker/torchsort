166d999e983a74af3a07e4a9a0c8c26c4cc449bb
Teddy Koker <teddy.koker@gmail.com>
2021-04-24 23:28:39 -0400

    default architecture for cuda

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ def ext_modules():
             cpp_extension.CUDAExtension(
                 "torchsort.isotonic_cuda",
                 sources=["torchsort/isotonic_cuda.cu"],
-            )
+            ),
+            extra_compile_args=["-arch=compute_50"],
         )
     return extensions
 
